8b5e0a0c8dd45874f9ab337561240f3ea5320830
manar843 <manarzaitoon93@gmail.com>
2025-01-27 14:24:07 +0200

    done colors

---
+++ lib/utils/app_color.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color  redColor  = Color (0xffE82626) ;
+  static const Color  orangeColor  = Color (0xffFFBB3B) ;
+  static const Color  darkGrayColor = Color (0xff282A28  ) ;
+  static const Color  whiteColor = Color (0xffFFFFFF  ) ;
+  static const Color  blackColor  = Color (0xff121312  ) ;
+  static const Color  greenColor  = Color (0xff57AA53 ) ;
+}
