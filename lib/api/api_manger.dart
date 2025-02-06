62ff659fb5a30bfeb10ece7119f0d6b948cfc680
marinageorge225 <georgemarina928@gmail.com>
2025-02-06 15:38:31 +0200

    added the 1st half of Home_Tab

--- lib/api/api_manger.dart
+++ lib/api/api_manger.dart
@@ -3,7 +3,7 @@ import 'package:graduation_movie_app/api/api_constant.dart';
 import 'package:graduation_movie_app/api/end_points.dart';
 import 'package:http/http.dart' as http;
 import '../model/LoginResponse.dart';
-
+import '../model/MovieResponse.dart';
 
 class ApiManager {
   final Uri url = Uri.parse(ApiConstant.urlLoginAuth);
@@ -32,6 +32,27 @@ class ApiManager {
       }
     } catch (e) {
       throw Exception('Error occurred while making API request: $e');
+    }
+  }
+
+//https://yts.mx/api/v2/list_movies.json
+
+  static Future<MovieResponse?> getMovies() async {
+    Uri url = Uri.https(ApiConstant.baseUrl, EndPoints.movieApi, {
+      'sort_by': 'date_added',
+      'order_by': 'desc',
+    });
+
+    try {
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        var json = jsonDecode(response.body);
+        return MovieResponse.fromJson(json);
+      } else {
+        throw Exception('Failed to load movies: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error occurred while fetching movies: $e');
     }
   }
 
