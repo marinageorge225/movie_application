da999f111ba542969bf372e0ce719e55b1f5f05f
marinageorge225 <georgemarina928@gmail.com>
2025-02-08 16:48:15 +0200

    sign in with google

--- lib/ui/home_screen/home_screen.dart
+++ lib/ui/home_screen/home_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:graduation_movie_app/ui/home_screen/tabs/browse_tab/browse_tab_widget.dart';
-import 'package:graduation_movie_app/ui/home_screen/tabs/home_tab/cubit/home_tab_widget.dart';
-import 'package:graduation_movie_app/ui/home_screen/tabs/profile/profile_tab_widget.dart';
+import 'package:graduation_movie_app/ui/home_screen/tabs/home_tab/home_tab_widget.dart';
 import 'package:graduation_movie_app/ui/home_screen/tabs/profile/update_profile.dart';
 import 'package:graduation_movie_app/ui/home_screen/tabs/search_tab/search_tab_widget.dart';
+
 import '../../core/utils/app_color.dart';
 import '../../core/utils/assets_manager.dart';
 
