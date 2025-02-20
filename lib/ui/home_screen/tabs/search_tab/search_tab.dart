d46f390ca97db5f63675e33bc0bdf3be3b4a3a91
marinageorge225 <georgemarina928@gmail.com>
2025-02-21 01:35:37 +0200

    navigate from search to details screen

--- lib/ui/home_screen/tabs/search_tab/search_tab.dart
+++ lib/ui/home_screen/tabs/search_tab/search_tab.dart
@@ -7,6 +7,7 @@ import 'package:graduation_movie_app/model/MovieListResponse.dart'
     as ListResponse;
 import 'package:graduation_movie_app/ui/home_screen/tabs/search_tab/cubit/search_tab_states.dart';
 import 'package:graduation_movie_app/ui/home_screen/tabs/search_tab/cubit/search_tab_view_model.dart';
+import 'package:graduation_movie_app/ui/movie_detailes_screen/movieDetails.dart';
 
 import '../../../widgets/custom_text_field.dart';
 
@@ -84,47 +85,58 @@ class _SearchTabState extends State<SearchTab> {
         itemCount: movies.length,
         itemBuilder: (context, index) {
           final movie = movies[index];
-          return Container(
-            margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-            decoration: BoxDecoration(
-              color: AppColors.greenColor,
-              borderRadius: BorderRadius.circular(12),
-            ),
-            child: Stack(
-              children: [
-                ClipRRect(
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MovieDetails(movieId: movie.id!),
+                ),
+              );
+            },
+            child: Container(
+              margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+              decoration: BoxDecoration(
+                color: AppColors.greenColor,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Stack(
+                children: [
+                  ClipRRect(
                     borderRadius: BorderRadius.circular(12),
                     child: Image.network(
                       movie.largeCoverImage!,
                       width: double.infinity,
                       height: double.infinity,
                       fit: BoxFit.cover,
-                    )),
-                Positioned(
-                  top: 8,
-                  left: 8,
-                  child: Container(
-                    padding:
-                        const EdgeInsets.symmetric(horizontal: 6, vertical: 4),
-                    decoration: BoxDecoration(
-                      color: Colors.black.withOpacity(0.7),
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        const Icon(Icons.star,
-                            color: AppColors.orangeColor, size: 14),
-                        const SizedBox(width: 4),
-                        Text(
-                          "${movie.rating}",
-                          style: AppStyles.regular14WhiteRoboto,
-                        ),
-                      ],
                     ),
                   ),
-                ),
-              ],
+                  Positioned(
+                    top: 8,
+                    left: 8,
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 6, vertical: 4),
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.7),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(Icons.star,
+                              color: AppColors.orangeColor, size: 14),
+                          const SizedBox(width: 4),
+                          Text(
+                            "${movie.rating}",
+                            style: AppStyles.regular14WhiteRoboto,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           );
         },
