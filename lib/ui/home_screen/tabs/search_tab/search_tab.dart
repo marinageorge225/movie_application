90cc12a6294fc79f65d7184a47ff445247cbf3b9
marinageorge225 <georgemarina928@gmail.com>
2025-02-21 02:00:19 +0200

    update search tab

--- lib/ui/home_screen/tabs/search_tab/search_tab.dart
+++ lib/ui/home_screen/tabs/search_tab/search_tab.dart
@@ -5,9 +5,9 @@ import 'package:graduation_movie_app/core/utils/app_color.dart';
 import 'package:graduation_movie_app/core/utils/app_styles.dart';
 import 'package:graduation_movie_app/model/MovieListResponse.dart'
     as ListResponse;
+import 'package:graduation_movie_app/ui/home_screen/tabs/home_tab/movie_item.dart';
 import 'package:graduation_movie_app/ui/home_screen/tabs/search_tab/cubit/search_tab_states.dart';
 import 'package:graduation_movie_app/ui/home_screen/tabs/search_tab/cubit/search_tab_view_model.dart';
-import 'package:graduation_movie_app/ui/movie_detailes_screen/movieDetails.dart';
 
 import '../../../widgets/custom_text_field.dart';
 
@@ -74,71 +74,68 @@ class _SearchTabState extends State<SearchTab> {
   }
 
   Widget buildMovieList(List<ListResponse.Movie> movies) {
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
     return Expanded(
       child: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          crossAxisSpacing: 5,
-          mainAxisSpacing: 7,
-          childAspectRatio: 0.6,
+          crossAxisSpacing: 9,
+          mainAxisSpacing: 10,
+          childAspectRatio: 0.65,
         ),
         itemCount: movies.length,
         itemBuilder: (context, index) {
           final movie = movies[index];
-          return InkWell(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => MovieDetails(movieId: movie.id!),
-                ),
-              );
-            },
-            child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
-              decoration: BoxDecoration(
-                color: AppColors.greenColor,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Stack(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(12),
-                    child: Image.network(
-                      movie.largeCoverImage!,
-                      width: double.infinity,
-                      height: double.infinity,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Positioned(
-                    top: 8,
-                    left: 8,
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 6, vertical: 4),
-                      decoration: BoxDecoration(
-                        color: Colors.black.withOpacity(0.7),
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          const Icon(Icons.star,
-                              color: AppColors.orangeColor, size: 14),
-                          const SizedBox(width: 4),
-                          Text(
-                            "${movie.rating}",
-                            style: AppStyles.regular14WhiteRoboto,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+          return MovieItem(
+            movie: movie,
+            imageHeight: 0.45 * height,
+            imageWidth: 0.5 * width,
           );
+          //Container(
+          //   margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+          //   decoration: BoxDecoration(
+          //     color: AppColors.greenColor,
+          //     borderRadius: BorderRadius.circular(12),
+          //   ),
+          //   child: Stack(
+          //     children: [
+          //       ClipRRect(
+          //         borderRadius: BorderRadius.circular(12),
+          //         child: Image.network(
+          //           movie.largeCoverImage!,
+          //           width: double.infinity,
+          //           height: double.infinity,
+          //           fit: BoxFit.cover,
+          //         ),
+          //       ),
+          //       Positioned(
+          //         top: 8,
+          //         left: 8,
+          //         child: Container(
+          //           padding: const EdgeInsets.symmetric(
+          //               horizontal: 6, vertical: 4),
+          //           decoration: BoxDecoration(
+          //             color: Colors.black.withOpacity(0.7),
+          //             borderRadius: BorderRadius.circular(8),
+          //           ),
+          //           child: Row(
+          //             mainAxisSize: MainAxisSize.min,
+          //             children: [
+          //               const Icon(Icons.star,
+          //                   color: AppColors.orangeColor, size: 14),
+          //               const SizedBox(width: 4),
+          //               Text(
+          //                 "${movie.rating}",
+          //                 style: AppStyles.regular14WhiteRoboto,
+          //               ),
+          //             ],
+          //           ),
+          //         ),
+          //       ),
+          //     ],
+          //   ),
+          // ),
         },
       ),
     );
